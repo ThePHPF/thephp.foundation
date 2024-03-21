8f4c6bae59a9fb3a2c01e822a373ff954797d7b1
Roman Pronskiy <roman@pronskiy.com>
2024-03-21 16:25:35 +0100

    Add Mermaid diagrams converter

--- app/SculpinKernel.php
+++ app/SculpinKernel.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Bundles\AtomFeedGeneratorBundle\SculpinAtomFeedGeneratorBundle;
+use App\Bundles\MermaidBundle\SculpinMermaidBundle;
 use App\Bundles\SharingImageGeneratorBundle\SculpinSharingImageGeneratorBundle;
 use Sculpin\Bundle\SculpinBundle\HttpKernel\AbstractKernel;
 
@@ -10,7 +11,8 @@ class SculpinKernel extends AbstractKernel
     {
         return [
             SculpinAtomFeedGeneratorBundle::class,
-            SculpinSharingImageGeneratorBundle::class
+            SculpinSharingImageGeneratorBundle::class,
+            SculpinMermaidBundle::class,
         ];
     }
 }
